a9e8e26881d82eef2bdaea109734f5dc3774ba68
genocider34 <alonzoball07@gmail.com>
2024-01-24 17:49:50 +0800

    test

--- practice/practice-script.js
+++ practice/practice-script.js
@@ -1,6 +1,23 @@
-const button = document.querySelector("button");
-const tasks = [];
-button.addEventListener("click", () => {
-   const id = tasks.length + 1;
-   
-});
+const arr = [
+  {
+    id: 1,
+    task: "hello",
+  },
+  {
+    id: 2,
+    task: "wow",
+  },
+  {
+    id: 3,
+    task: "world",
+  },
+];
+
+const deletedID = 3;
+const filteredID = arr.filter((item) => item.id !== deletedID);
+// const filtered = arr.filter((item) => item.id <= 1);
+// const mapped = arr.map((item) => item.task);
+
+console.log(filteredID);
+// console.log(mapped);
+// console.log(filtered);
