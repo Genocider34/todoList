dabd87e22633a9051ed59f6a60b3e5048fc977b2
genocider34 <alonzoball07@gmail.com>
2024-01-25 09:33:15 +0800

    delete function finally working!

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const form = document.querySelector("form");
 const ul = document.querySelector("ul");
 let data = [];
-function dataCheck() {
-  data.forEach((x) => {
-    console.log(x);
-  });
-}
+let tempID = 1;
+
+// function dataCheck() {
+//   data.forEach((x) => {
+//     console.log(x);
+//   });
+// }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -13,69 +15,54 @@ form.addEventListener("submit", (e) => {
 });
 
 function userValidation() {
-  const inputUser = form.elements[0].value;
+  const inputUser = form.elements[0].value; // string
   let newLI;
   let trashIcon;
   if (inputUser.trim() !== "") {
-    newLI = document.createElement("LI");
+    newLI = document.createElement("LI"); //li
     trashIcon = document.createElement("i");
     trashIcon.classList.add("fa-solid", "fa-trash");
+    editIcon = document.createElement("i");
+    editIcon.classList.add("fa-solid", "fa-pen-to-square");
 
-    newLI.innerText = inputUser;
+    newLI.innerText = inputUser; //li
+
+    newLI.appendChild(editIcon);
     newLI.appendChild(trashIcon);
     ul.appendChild(newLI);
     form.elements[0].value = "";
     createData(inputUser);
-    // dataCheck();
   }
-  deleteData(trashIcon, newLI);
-  // completedData(newLI);
+
+  completedData(newLI, tempID);
+  deleteData(trashIcon, tempID);
 }
 
-// function updateData(task) {}
+// function updateData(task, input) {
+//   task.innerText =
+// }
 
-function deleteData(icon, li) {
-  data.isDeleted = true;
-  icon.addEventListener("click", () => {
-    icon.parentElement.remove(); // deletes the append LI
-    /* 
-    This space deletes the selected data object when the event is click
-    ->use the boolean
-    */
-    const itemId = li.dataset.itemId; // get the ID of the item to be deleted from the dataset
-    data = data.filter((item) => item.id !== parseInt(itemId)); // filter out the item with the matching ID
-    console.log(data); // log the updated data array
-    console.log(data);
-  });
-}
-
-// Currently in-progress
-function completedData(task) {
-  task.addEventListener("click", () => {
-    const deleteID = 1;
-    data.filter((item) => item.id !== deleteID);
-    console.log(data);
-
-    task.remove(); // deletes the append LI
-
-    /* 
-    This space deletes the selected data object when the event is click
-    ->use the boolean
-    */
-  });
-}
-
-// Done
 function createData(input) {
-  let idAdd = 1;
   for (let i = 0; i < data.length; i++) {
-    idAdd = data[i].id + 1;
+    tempID = data[i].id + 1;
   }
   data.push({
-    id: idAdd,
+    id: tempID,
     item: input,
-    isDeleted: false,
   });
 
   // localStorage.setItem("dataLocal", JSON.stringify(data));
+}
+function deleteData(icon, id) {
+  icon.addEventListener("click", () => {
+    icon.parentElement.remove(); // deletes the append LI
+
+    data = data.filter((item) => item.id !== id);
+  });
+}
+function completedData(task, id) {
+  task.addEventListener("click", () => {
+    task.remove(); // deletes the append LI
+    data = data.filter((item) => item.id !== id);
+  });
 }
